950dd283da221376634b86eedeb2cf9417a69353
riskatikah <riskaatika88@gmail.com>
2025-07-22 15:40:09 +0700

    cleanup

--- backend/engineering_app/models.py
+++ backend/engineering_app/models.py
@@ -9,10 +9,18 @@ class UserProfile(models.Model):
         ('qac', 'QAC/Compliance'),
         ('requester', 'Division Req'),
     ]
+    STATUS_CHOICES = [
+        ('Active', 'Active'),
+        ('Inactive', 'Inactive'),
+        ('Pending', 'Pending'),
+    ]
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile')
+    full_name = models.CharField(max_length=100)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
-    division = models.CharField(max_length=100, blank=True, null=True)
+    division = models.CharField(max_length=50)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    avatar = models.URLField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.user.username} ({self.role})"
+        return self.full_name
